6c0a57a12090ec7dff985b2e3ab84e7eca1d78fb
wat <twatcharin90@gamil.com>
2023-01-31 17:49:10 +0700

    fix: edit text

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import News from "./components/News";
-import Navbar from "./components/Navbar";
 import ActivityPage from "./pages/ActivityPage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
@@ -14,7 +12,6 @@ function App() {
         <Route path="/" element={<ActivityPage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/:id" element={<EditPage />} />
-        <Route path="/register" element={<News />} />
         <Route path="*" element={<h1>Page Not Found.</h1>} />
       </Routes>
     </Router>
